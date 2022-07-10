ab4bd04d84d025dbe6d9d922198d9db28a5905bb
SK <109010254+SatsukiKobayashi@users.noreply.github.com>
2022-07-11 07:49:31 +0900

    about_templateString

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import "./styles.css";
+/**
+ * テンプレート文字列（文字列の中に変数を埋め込める）
+ */
+const name = "neko";
+const age = "28";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-<p>test</p>
-`;
+//before
+const before = "I'm " + name + ". Age is " + age;
+console.log(before);
+
+//Use templateString
+const useTemplateString = `This is using templateString.I'm ${name}.Age is ${age}`;
+console.log(useTemplateString);
