43c5a22920f6724283f2931b1015371a1b05ba8e
SK <109010254+SatsukiKobayashi@users.noreply.github.com>
2022-07-11 13:27:20 +0900

    aboutArrowFunction(included Omitted)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import "./styles.css";
+/**
+ * arrow fanction
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-<p>test</p>
-`;
+//before
+function funcBefore(anyStr) {
+  return anyStr;
+}
+//関数を呼び出して返却結果をコンソールに表示
+console.log(funcBefore("funcBefore"));
+
+const funcInVariable = function (aStr) {
+  return aStr;
+};
+console.log(funcInVariable("funcInVariable"));
+
+//use arrow fanction
+//引数のカッコはなくてもいい（ただ保存すると自動整形ツールで勝手に補完される）
+const funcUsingArrow = (arrwStr) => {
+  return arrwStr;
+};
+console.log(funcUsingArrow("funcUsingArrow"));
+
+//Arrow function 省略形
+//returnを書かなくてもいい
+const funcOmittedArrow = (omitStr) => omitStr;
+console.log(`****funcOmittedArrow****${funcOmittedArrow("funcOmittedArrow")}`);
+
+//数値のたし算（省略なし）
+const funcCalclate = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(`*******funcCalclate******=> ${funcCalclate(2, 2)}`);
+
+//数値のたし算（省略あり）
+const omittedFuncCalc = (num1, num2) => num1 + num2;
+console.log(`omittedFuncCalc=> ${omittedFuncCalc(22, 22)}`);
